5aa9615e2c069b3fb406eb2d4791dde242a7edfb
MichelAD1 <michel.a.abidaoud@gmail.com>
2022-09-16 16:36:19 +0300

    Changed output of usd and lbp converters to textview

--- app/src/main/java/com/example/assignment_1/LBPConverterActivity.java
+++ app/src/main/java/com/example/assignment_1/LBPConverterActivity.java
@@ -3,16 +3,18 @@ package com.example.assignment_1;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class LBPConverterActivity extends AppCompatActivity {
-
+    TextView usd_f_amount;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lbpconverter);
+        usd_f_amount=(TextView) findViewById(R.id.usd_amount);
     }
     public void convertLBP(View v){
         double usd_final=0;
@@ -20,6 +22,6 @@ public class LBPConverterActivity extends AppCompatActivity {
         String lbp_val=lbp.getText().toString();
         double lbp_final=Double.parseDouble(lbp_val);
         usd_final=lbp_final/40000;
-        Toast.makeText(this, "USD: "+usd_final, Toast.LENGTH_LONG).show();
+        usd_f_amount.setText("USD: "+usd_final);
     }
 }
